1ce24eed9f4a856ae46a9e3ef971e270485f632d
Laurie Young <laurie@wildfalcon.com>
2018-07-20 22:29:33 +0100

    Removed unnecessary call to self

--- lib/array.rb
+++ lib/array.rb
@@ -1,6 +1,6 @@
 # Reopening Array in order to create a demonstration of how to do so
 class Array
   def pair_that_sums_to?(total)
-    ArrayPairSummer.new(self.sort).pair_that_sums_to?(total)
+    ArrayPairSummer.new(sort).pair_that_sums_to?(total)
   end
 end
