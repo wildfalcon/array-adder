eb82d9dfb7cce52f8560f65f0d7d82e5a9f8dd28
Laurie Young <laurie@wildfalcon.com>
2018-07-20 19:08:49 +0100

    Added trivial success case
    (when first and last element sum to total)

--- lib/array.rb
+++ lib/array.rb
@@ -1,6 +1,9 @@
 # Reopening Array in order to create a demostration of how to do so
 class Array
-  def pair_that_sums_to?(_total)
-    false
+  def pair_that_sums_to?(total)
+    low_pointer = 0
+    high_pointer = length - 1
+
+    (self[low_pointer] + self[high_pointer]) == total
   end
 end
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -5,5 +5,9 @@ describe 'Array' do
     it 'returns false when no pair adds to total' do
       expect([1, 2, 3, 4].pair_that_sums_to?(8)).to eq false
     end
+
+    it 'returns true when a pair adds to total' do
+      expect([1, 2, 4, 4].pair_that_sums_to?(5)).to eq true
+    end
   end
 end
