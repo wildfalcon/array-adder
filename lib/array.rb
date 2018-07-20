24148139a80932c24fe02d22074ae0d3236cc6af
Laurie Young <laurie@wildfalcon.com>
2018-07-20 22:17:14 +0100

    Delegated Array#pair_that_sums_to? to ArrayPairSummer

--- lib/array.rb
+++ lib/array.rb
@@ -1,36 +1,8 @@
+require 'lib/array_pair_summer'
+
 # Reopening Array in order to create a demonstration of how to do so
 class Array
   def pair_that_sums_to?(total)
-    raise StandarError if sort != self
-
-    pointers = [0, length - 1]
-
-    while more_moves_available?(pointers)
-      break if current_total(pointers) == total
-      next_element_pair(pointers, total)
-    end
-
-    current_total(pointers) == total
-  end
-
-  def next_element_pair(pointers, total)
-    next_lowest_total(pointers) if current_total(pointers) > total
-    next_highest_total(pointers) if current_total(pointers) < total
-  end
-
-  def more_moves_available?(pointers)
-    (pointers.last - pointers.first) > 1
-  end
-
-  def current_total(pointers)
-    self[pointers.first] + self[pointers.last]
-  end
-
-  def next_lowest_total(pointers)
-    pointers[1] = pointers[1] - 1
-  end
-
-  def next_highest_total(pointers)
-    pointers[0] = pointers[0] + 1
+    ArrayPairSummer.new(self).pair_that_sums_to?(total)
   end
 end
